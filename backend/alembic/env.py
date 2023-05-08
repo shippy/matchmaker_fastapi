7c83d44a6a6dfdfbc2d80029b368141b697bcbf5
Simon Podhajsky <simon.podhajsky@gmail.com>
2023-05-08 20:35:13 +0200

    Fix Alembic env to use SQLModel

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -2,6 +2,7 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
+from sqlmodel import SQLModel
 
 from alembic import context
 
@@ -16,9 +17,9 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-# from myapp import mymodel
+from models.questionnaire import *
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = SQLModel.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
