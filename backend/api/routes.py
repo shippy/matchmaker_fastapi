c3a636a1acacd04aa411ae6885a7ccbaacb8fa48
Simon Podhajsky <simon.podhajsky@gmail.com>
2023-05-11 08:52:17 +0200

    Try (&fail) to return Questionnaire with child q's

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -1,14 +1,19 @@
 from fastapi import APIRouter, Depends, HTTPException
-from backend.models.questionnaire import Questionnaire
+from backend.models.questionnaire import Questionnaire, Question
 from backend.schemas.questionnaire import QuestionnaireCreate
 from backend.core.database import get_session
 from sqlmodel import Session
+from sqlalchemy.orm import joinedload
 
 router = APIRouter()
 
 @router.get("/questionnaire/{questionnaire_id}", response_model=Questionnaire)
-async def get_questionnaire(questionnaire_id: int, session: Session = Depends(get_session)):
+def get_questionnaire(questionnaire_id: int, session: Session = Depends(get_session)):
     questionnaire = session.get(Questionnaire, questionnaire_id)
+    # questionnaire = session.query(Questionnaire).options(joinedload(Questionnaire.questions)).filter(Questionnaire.id == questionnaire_id).first()
+
+    questions = session.query(Question).where(Question.questionnaire_id == questionnaire_id).all()
     if questionnaire is None:
         raise HTTPException(status_code=404, detail="Questionnaire not found")
+    questionnaire.questions = questions
     return questionnaire
