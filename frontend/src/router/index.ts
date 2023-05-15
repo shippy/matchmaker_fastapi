257f322588ed1f6fd727daf090cad60d56da3020
Simon Podhajsky <simon.podhajsky@gmail.com>
2023-05-15 03:54:56 +0200

    Add a template for questionnaire loading

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -16,6 +16,13 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    // Create a view for a specific questionnaire ID retrieved from 
+    // the backend at /api/questionnaire/:id.
+    {
+      path: '/questionnaire/:id',
+      name: 'questionnaire',
+      component: () => import('../views/QuestionnaireView.vue')
     }
   ]
 })
