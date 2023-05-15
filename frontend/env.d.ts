f94d3329861a57b19f60428b2bab5c1abbcc4706
Simon Podhajsky <simon.podhajsky@gmail.com>
2023-05-15 05:31:13 +0200

    Fix TS complaints

--- frontend/env.d.ts
+++ frontend/env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+
+// https://stackoverflow.com/a/73557254/2114580
+declare module '*.vue' {
+    import type { DefineComponent } from 'vue';
+    const component: DefineComponent<{}, {}, any>;
+    export default component;
+}
+  
--- frontend/src/views/QuestionnaireView.vue
+++ frontend/src/views/QuestionnaireView.vue
@@ -1,46 +1,71 @@
 <template>
-    <div>
-      <div v-if="loading">Loading...</div>
-      <div v-else-if="error">{{ error }}</div>
-      <div v-else>
-        <h1>{{ questionnaire.title }}</h1>
-        <div v-for="question in questionnaire.questions" :key="`question-${question.id}`">
-          <h2>{{ question.text }}</h2>
-          <ul>
-            <li v-for="answer in question.answers" :key="`answer-${answer.id}`">
-              {{ answer.text }}
-            </li>
-          </ul>
-        </div>
+  <div>
+    <div v-if="loading">Loading...</div>
+    <div v-else-if="error">{{ error }}</div>
+    <div v-else-if="questionnaire">
+      <h1>{{ questionnaire.title }}</h1>
+      <div v-for="question in questionnaire.questions" :key="`question-${question.id}`">
+        <h2>{{ question.text }}</h2>
+        <ul>
+          <li v-for="answer in question.answers" :key="`answer-${answer.id}`">
+            {{ answer.text }}
+          </li>
+        </ul>
       </div>
     </div>
-  </template>
+  </div>
+</template>
   
-  <script>
-  import axios from 'axios'
-  
-  export default {
-    data() {
-      return {
-        questionnaire: null,
-        loading: true,
-        error: null
-      }
-    },
-    async created() {
-      try {
-        const response = await axios.get(`/api/questionnaires/${this.$route.params.id}`)
-        this.questionnaire = response.data
-        this.loading = false
-      } catch (err) {
-        this.error = err.message
-        this.loading = false
-      }
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { type RouteLocation } from "vue-router"
+import axios from 'axios'
+import Vue from 'vue'
+
+declare module "@vue/runtime-core" {
+  interface ComponentCustomProperties {
+    $route: RouteLocation;
+  }
+}
+
+interface Answer {
+  id: number;
+  text: string;
+}
+
+interface Question {
+  id: number;
+  text: string;
+  answers: Answer[];
+}
+
+interface Questionnaire {
+  id: number;
+  title: string;
+  questions: Question[];
+}
+
+export default defineComponent({
+  data() {
+    return {
+      questionnaire: null as Questionnaire | null,
+      loading: true,
+      error: null as string | null
+    }
+  },
+  async created() {
+    try {
+      const response = await axios.get<Questionnaire>(`/api/questionnaires/${this.$route.params.id}`)
+      this.questionnaire = response.data
+      this.loading = false
+    } catch (err: unknown) {
+      this.error = (err as Error).message
+      this.loading = false
     }
   }
-  </script>
-  
-  <style>
-  /* Your styles here */
-  </style>
-  
+})
+</script>
+
+<style>
+/* Your styles here */
+</style>
